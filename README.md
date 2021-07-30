5ec8a9d56f68f4f3d81cedb045d94dcf4cad7370
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2021-07-30 13:20:31 +1000

    Feat: redeploy to kovan

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # ddemonstrate.contracts
 Smart contracts for ddemonstrate.org
 
-Kovan: `0x1D13fFe227cAC43E33d575D5B32E0991c28ac249`
+Token Kovan: `0x5dbdD6F1A26C5549AcDC50f1f3d37ca5F4D99291`
+
+https://kovan.etherscan.io/address/0x5dbdD6F1A26C5549AcDC50f1f3d37ca5F4D99291#contracts
+
+Demonstrate Kovan: `0xac3D9627C1267f7f143DCF2Fd3Aa17116dc668cB`
+
+https://kovan.etherscan.io/address/0xac3D9627C1267f7f143DCF2Fd3Aa17116dc668cB#contracts
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,10 @@ const Token = artifacts.require("Token");
 
 module.exports = async (deployer) => {
   await deployer.deploy(Demonstrate);
-  const instance = await Demonstrate.deployed();
-  console.log("Deployed contract:", instance);
-  await deployer.deploy(Token(instance.address));
+  const dd = await Demonstrate.deployed();
+  console.log(`Deployed contract: ${dd.address}`);
+  
+  await deployer.deploy(Token, dd.address);
+  const token = await Token.deployed();
+  console.log(`Deployed contract: ${token.address}`);
 };
